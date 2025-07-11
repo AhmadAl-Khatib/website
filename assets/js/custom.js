1d0329d2c32006ba75347d090127a125d1afa13e
Ahmad Al Khatib <126989753+AhmadAl-Khatib@users.noreply.github.com>
2025-07-11 18:44:52 +0300

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,30 +1,3 @@
-// cv display function
-function openCVModal() {
-  const modal = document.getElementById('cvModal');
-  const container = document.getElementById('cvIframeContainer');
-  if (!container.querySelector('iframe')) {
-    const iframe = document.createElement('iframe');
-    iframe.src = 'Career.pdf';
-    iframe.width = '100%';
-    iframe.height = '600px';
-    iframe.style.border = 'none';
-    container.appendChild(iframe);
-  }
-  modal.style.display = 'block';
-}
-function closeCVModal() {
-  const modal = document.getElementById('cvModal');
-  modal.style.display = 'none';
-  const container = document.getElementById('cvIframeContainer');
-  container.innerHTML = '';
-}
-window.addEventListener('click', function (event) {
-  const modal = document.getElementById('cvModal');
-  if (event.target === modal) {
-    closeCVModal();
-  }
-});
-
 // Get today's date
 document.addEventListener('DOMContentLoaded', () => {
     const today = new Date();
